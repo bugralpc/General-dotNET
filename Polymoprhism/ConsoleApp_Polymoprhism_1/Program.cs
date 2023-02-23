850823ab7758de46b7762aeea4d8beecd3796a26
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-02-23 14:21:54 +0300

    update

---
+++ Polymoprhism/ConsoleApp_Polymoprhism_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp_Polymoprhism_1
+{
+    // URLs
+    // https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/object-oriented/polymorphism
+    internal class Program
+    {
+        // In C#, every type is polymorphic because all types, including user-defined types, inherit from Object.
+
+        // Polymorphism is third-pillar of oop after encapsulation and inheritance
+
+        // At run time, objects of a derived class may be treated as objects of a base class in places such as
+        // method parameters and collections or arrays.
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>
+            {
+                new Rectangle(),
+                new Triangle(),
+                new Circle()
+            };
+
+            // the virtual method Draw is invoked on each of the derived class, not the base class.
+            foreach(var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
---
+++ Polymoprhism/ConsoleApp_Polymoprhism_1/Shape.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Polymoprhism_1
+{
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+            base.Draw();
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a rectangle");
+            base.Draw();
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a triangle");
+            base.Draw();
+        }
+    }
+}
