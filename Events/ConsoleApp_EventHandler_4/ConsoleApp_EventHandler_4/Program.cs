f57c07bb35486c5f094ef98ab51ad34aac7abc74
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-02 23:03:55 +0300

    Main Thread invokes event, worker thread invokes event

--- Events/ConsoleApp_EventHandler_4/ConsoleApp_EventHandler_4/Program.cs
+++ Events/ConsoleApp_EventHandler_4/ConsoleApp_EventHandler_4/Program.cs
@@ -1,10 +1,54 @@
 ﻿namespace ConsoleApp_EventHandler_4
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Process += DoProcess1;
+
+            // First run on Main Thread.
+            DoWork1();
+
+            Thread.Sleep(500);
+
+            // Then run on Worker Thread.
+            Thread thread = new Thread(() =>
+            {
+                DoWork1();
+            });
+
+            thread.Start();
+
+
         }
+
+        public delegate void Notify();
+
+        public static event Notify Process;
+
+        public static void DoWork2()
+        {
+            Process?.Invoke();
+        }
+
+        public static void DoWork1()
+        {
+            for (int i = 0; i < 5; i++ )
+            {
+                Console.WriteLine("Current Thread => {0} ", Thread.CurrentThread.ManagedThreadId + " DoWork1 Method.");
+            }
+
+            DoWork2();
+        }
+
+        public static void DoProcess1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Current Thread => {0} ", Thread.CurrentThread.ManagedThreadId + " DoProcess1 Method");
+            }
+        }
+
     }
 }
