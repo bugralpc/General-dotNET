331dfbd9e1649307a6dffd28e8ad5c8eddf2f3f4
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-02 22:20:33 +0300

    publisher object, subscriber main method

---
+++ Events/ConsoleApp_EventHandler_2/ConsoleApp_EventHandler_2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp_EventHandler_2
+{
+
+    // Passing data by extending EventArgs
+    // To pass data in the second parameter of the vent handler from publisher to subscriber, you need to define a class
+    // that inherits the EventArgs class.
+
+    public class OrderEventArgs: EventArgs
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class Order
+    {
+        public event EventHandler<OrderEventArgs> OnCreated;
+
+        public void Create(string email, string phone)
+        {
+            Console.WriteLine("Order Created");
+
+            if (OnCreated != null)
+            {
+                OnCreated(this, new OrderEventArgs { Email = email, Phone = phone });
+            }
+        }
+    }
+
+    class Email
+    {
+        public static void Send(object sender, OrderEventArgs e)
+        {
+            Console.WriteLine($"Send an email to {e.Email}");
+        }
+    }
+
+    class SMS
+    {
+        public static void Send(object sender, OrderEventArgs e)
+        {
+            Console.WriteLine($"Send an SMS to {e.Phone}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order();
+
+            order.OnCreated += Email.Send;
+            order.OnCreated += SMS.Send;
+
+            order.Create("john@test.com", "(408)-111-2222");
+        }
+    }
+}
---
+++ Events/ConsoleApp_EventHandler_3/ConsoleApp_EventHandler_3/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp_EventHandler_3
+{
+    public delegate void Notify(); // delegate
+
+    public class ProcessBusinessLogic 
+    {
+        public event Notify Process; // Declare an event in publisher class
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+
+            // if Process is not null then call delegate
+            Process?.Invoke();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The program class is a subscriver of the Process event. It register with the event using += operator
+
+            // ProcessBusinessLogic class is called the publisher.
+            ProcessBusinessLogic processBusinessLogic = new ProcessBusinessLogic();
+
+            // all the event handler methods registered with the Process event.
+            processBusinessLogic.Process += Method1;
+            processBusinessLogic.Process += Method2;
+
+            // raising the Process event.
+            processBusinessLogic.StartProcess();
+
+
+        }
+
+        private static void Method1()
+        {
+            Console.WriteLine("Process1 completed");
+        }
+        private static void Method2()
+        {
+            Console.WriteLine("Process2 completed");
+        }
+    }
+}
