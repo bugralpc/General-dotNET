fe00b8f7aec1fc6a577adcadb701db402fceaf1c
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-13 22:05:06 +0300

    commit and push

---
+++ Thread/ConsoleApp_TaskExceptionHandling/ConsoleApp_TaskExceptionHandling/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ConsoleApp_TaskExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task1 = Task.Factory.StartNew(() =>
+            {
+                throw new InvalidOperationException("Cant do this") { Source = "task1" };
+            });
+
+            var task2 = Task.Factory.StartNew(() =>
+            {
+                throw new AccessViolationException("Cant do this") { Source = "task2" };
+            });
+
+            try
+            {
+                Task.WaitAll(task1, task2);
+            }
+            catch(AggregateException ae)
+            {
+                foreach(var e in ae.InnerExceptions)
+                {
+                    Console.WriteLine("Exception {0} from {1}", e.GetType(), e.Source);
+                }
+            }
+
+            Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ \n\n");
+
+            try
+            {
+                Test();
+            }
+            catch(AggregateException ae)
+            {
+                foreach(var e in ae.InnerExceptions)
+                {
+                    Console.WriteLine($"Handled elsewhere: {e.GetType()}");
+                }
+            }
+            
+        }
+
+        private static void Test()
+        {
+            var task1 = Task.Factory.StartNew(() =>
+            {
+                throw new InvalidOperationException("Cant do this") { Source = "task1" };
+            });
+
+            var task2 = Task.Factory.StartNew(() =>
+            {
+                throw new AccessViolationException("Cant do this") { Source = "task2" };
+            });
+
+            try
+            {
+                Task.WaitAll(task1, task2);
+            }
+            catch(AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    if (e is InvalidOperationException)
+                    {
+                        Console.WriteLine("Invalid op");
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                });
+            }
+
+        }
+    }
+}
