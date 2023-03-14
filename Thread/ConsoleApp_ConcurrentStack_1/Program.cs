0a7ea14d1436bc737ad0eb31adbea7265c91f9f8
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-14 18:03:30 +0300

    commit and push

---
+++ Thread/ConsoleApp_ConcurrentStack_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConsoleApp_ConcurrentStack_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bag = new ConcurrentBag<int>();
+
+            var tasks = new List<Task>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                var i1 = i;
+
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    bag.Add(i1);
+                    Console.WriteLine($"{Task.CurrentId} has added {i1}");
+                    int result;
+                    if (bag.TryPeek(out result))
+                    {
+                        Console.WriteLine($"{Task.CurrentId} has peeked the value {result}");
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+        }
+    }
+}
