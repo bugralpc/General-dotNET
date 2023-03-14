5115934897344a79e9102b64dbec6111b339edcc
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-14 14:54:38 +0300

    commit and push

---
+++ Thread/ConsoleApp_ConcurrentQueue_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConsoleApp_ConcurrentQueue_2
+{
+    // Procuder-Consumer Design Pattern Example
+    internal class Program
+    {
+        static BlockingCollection<int> queue = new BlockingCollection<int>(boundedCapacity: 5);
+
+        static void Main(string[] args)
+        {
+            // Producer
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    queue.Add(i);
+                    Thread.Sleep(1000);
+                }
+
+                queue.CompleteAdding();
+            });
+
+            // Consumer
+            Task.Run(() =>
+            {
+                foreach(int item in queue.GetConsumingEnumerable())
+                {
+                    Console.WriteLine(item);
+                }
+                
+            });
+
+
+            Console.ReadKey();
+        }
+    }
+}
