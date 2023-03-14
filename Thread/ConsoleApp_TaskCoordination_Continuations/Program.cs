d9ab0e00d5f4b66f007818a6b9b98fc528fd9636
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-14 18:23:26 +0300

    commit and push

---
+++ Thread/ConsoleApp_TaskCoordination_Continuations/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp_TaskCoordination_Continuations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Boiling water");
+                Thread.Sleep(3000);
+            });
+
+            var task2 = task.ContinueWith(t =>
+            {
+                Console.WriteLine($"Completed task {t.Id}, pour water into the cup");
+            });
+
+            task2.Wait();
+
+
+            var task3 = Task.Factory.StartNew(() => "Task3");
+            var task4 = Task.Factory.StartNew(() => "Task4");
+
+            var task5 = Task.Factory.ContinueWhenAll(new[] { task3, task4 },
+                tasks =>
+                {
+                    Console.WriteLine("Tasks completed");
+                    foreach (var t in tasks)
+                    {
+                        Console.WriteLine(t.Result);
+                    }
+                    Console.WriteLine("All tasks done");
+
+                });
+
+            task5.Wait();
+        }
+
+    }
+}
