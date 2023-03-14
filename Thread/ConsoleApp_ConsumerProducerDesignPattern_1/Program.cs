6869588536a196848f62299aa05231b200d88807
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-14 15:14:31 +0300

    commit and push

---
+++ Thread/ConsoleApp_ConsumerProducerDesignPattern_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConsoleApp_ConsumerProducerDesignPattern_1
+{
+    // Consumer-Producer Design Pattern examples.
+
+    internal class Program
+    {
+        static BlockingCollection<int> queue = new BlockingCollection<int>(boundedCapacity: 10);
+
+        static TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+        static void Main(string[] args)
+        {
+            // Producer task
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    queue.Add(i);
+                    Thread.Sleep(300);
+                }
+                queue.CompleteAdding();
+            });
+
+            // Consumer task
+            Task.Run(() =>
+            {
+                foreach (int item in queue.GetConsumingEnumerable())
+                {
+                    Console.WriteLine(item);
+                }
+            });
+
+            Console.ReadKey();
+
+            // Producer task
+            Task.Run(() =>
+            {
+                int result = 42; // Some long-running operation
+                tcs.SetResult(result);
+            });
+
+            // Consumer task
+            Task.Run(async () =>
+            {
+                int result = await tcs.Task;
+                Console.WriteLine(result);
+            });
+
+            Console.ReadKey();
+
+        }
+    }
+}
