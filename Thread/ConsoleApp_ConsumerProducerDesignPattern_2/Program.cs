139b55607309882267f516bf8073503d39523c6f
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-14 15:16:56 +0300

    commit and push

---
+++ Thread/ConsoleApp_ConsumerProducerDesignPattern_2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp_ConsumerProducerDesignPattern_2
+{
+    // Consumer-Producer Design Pattern Example
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            object lockObj = new object();
+
+            // Producer task
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    lock (lockObj)
+                    {
+                        queue.Enqueue(i);
+                        Monitor.Pulse(lockObj);
+                    }
+                }
+            });
+
+            // Consumer task
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    int item;
+                    lock (lockObj)
+                    {
+                        while (queue.Count == 0)
+                        {
+                            Monitor.Wait(lockObj);
+                        }
+                        item = queue.Dequeue();
+                    }
+                    Console.WriteLine(item);
+                }
+            });
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
