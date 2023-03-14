ff33e171f4ca650865503c781b3c5f55a659086f
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-14 17:52:03 +0300

    commit and push

---
+++ Thread/ConsoleApp_ConcurrentDictionary_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConsoleApp_ConcurrentDictionary_1
+{
+    internal class Program
+    {
+        private static ConcurrentDictionary<string, string> capitals = new ConcurrentDictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+
+            //AddParis();
+
+            Task.Factory.StartNew(AddParis).Wait();
+
+            AddParis();
+
+        }
+
+        private static void AddParis()
+        {
+            bool success = capitals.TryAdd("France", "Paris");
+
+            string who = Task.CurrentId.HasValue ? ("Task " + Task.CurrentId) : "Main thread";
+
+            Console.WriteLine($"{who} {(success ? "added": "did not add")} the element");
+        }
+    }
+}
