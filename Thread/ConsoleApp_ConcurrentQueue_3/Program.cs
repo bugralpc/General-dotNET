a568d3526c238eea1406ac4f0571f5dfa557e4de
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-14 17:54:50 +0300

    commit and push

---
+++ Thread/ConsoleApp_ConcurrentQueue_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConsoleApp_ConcurrentQueue_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new ConcurrentQueue<int>();
+
+            q.Enqueue(1);
+            q.Enqueue(2);
+
+            int result;
+
+            if (q.TryDequeue(out result))
+            {
+                Console.WriteLine("Removed element is " + result);
+            }
+
+            if (q.TryPeek(out result))
+            {
+                Console.WriteLine("Front element is " + result);
+            }
+        }
+    }
+}
