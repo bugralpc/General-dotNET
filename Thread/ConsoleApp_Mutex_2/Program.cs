b3f18613380d1051e581b845a88c5c526edbd3b6
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-03-23 13:55:49 +0300

    mutex example

---
+++ Thread/ConsoleApp_Mutex_2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp_Mutex_2
+{
+    internal class Program
+    {
+        static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                Thread thread = new Thread(DoWork);
+                thread.Start();
+            }
+
+
+        }
+
+        static void DoWork()
+        {
+            mutex.WaitOne();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Current Thread => " + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(100);
+            }
+
+            mutex.ReleaseMutex();
+        }
+    }
+}
